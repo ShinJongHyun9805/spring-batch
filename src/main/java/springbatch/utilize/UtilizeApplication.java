f22d085b93de9cc8208c9abf22a4fba54f10d410
jhshin <jakie5803@gmail.com>
2023-07-07 09:54:15 +0900

    spring batch utilize commit

---
+++ src/main/java/springbatch/utilize/UtilizeApplication.java
@@ -0,0 +1,13 @@
+package springbatch.utilize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springbatch/utilize/UtilizeApplicationTests.java
@@ -0,0 +1,13 @@
+package springbatch.utilize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
